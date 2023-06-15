24b5552fe5f08875776e6076241758b777d72070
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-06-15 23:22:35 +0300

    Some small things added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        File file = new File("SECRET FILE.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        //check if "ignition key" is in position
+        if (file.exists()) {
+            System.out.println("Loading.");
+            System.out.println("Loading..");
+            System.out.println("Loading...");
+        } else {
+            System.out.println("Failed to load!\nError 001");
+            return;
+        }
+
+        System.out.println("Who is starting up the program?\n");
+        String userName = scanner.nextLine();
+
+//starting screen
+        try {
+            FileWriter welcome = new FileWriter("Welcome.txt");
+            welcome.write("Welcome, ");
+            welcome.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }}
