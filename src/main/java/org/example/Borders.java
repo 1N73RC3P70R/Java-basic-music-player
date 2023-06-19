87af3aa43cc4aa13fe308a0b602930907604de32
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-06-19 23:07:22 +0300

    Application UserInteraction Музыка [x]Need TODO controls []Граница

---
+++ src/main/java/org/example/Borders.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Borders {
+    public void Frame (){
+        JFrame frame = new JFrame();
+        frame.setTitle("Pulvinus");
+        frame.setVisible(true);
+        frame.setSize(800,600);
+
+    }
+}
